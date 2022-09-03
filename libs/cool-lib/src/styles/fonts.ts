6e37a53776f0d307a3037b3b5634204c77b42d4f
Russell Little <rrlittle2@gmail.com>
2022-09-03 10:54:06 -0500

    moved cool-lib to ui
    and added dev / dev:test

--- libs/cool-lib/src/styles/fonts.ts
+++
@@ -1,4 +0,0 @@
-export const bigSize = 20;
-export const normalSize = 12;
-export const heavyWeight = 'bold'
-export const normalWeight = 'normal' 
---
+++ libs/ui/src/styles/fonts.ts
@@ -0,0 +1,4 @@
+export const bigSize = 20;
+export const normalSize = 12;
+export const heavyWeight = "bold";
+export const normalWeight = "normal";
