d6e7adb3c8be5cdb12df02bf2ec0c0254925e222
Russell Little <rrlittle2@gmail.com>
2022-09-04 12:00:20 -0500

    everything is happy! huzzah!!

--- libs/ui/src/Header/index.stories.tsx
+++ libs/ui/src/Header/index.stories.tsx
@@ -1,7 +1,11 @@
 import { Header } from ".";
 
-const Template = ({ text }) => <Header text={text} />;
+export default {
+  title: "Header",
+  component: Header,
+};
+
+export const Template = ({ text }) => <Header text={text} />;
 Template.args = {
   text: "hello",
 };
-export default Template;
