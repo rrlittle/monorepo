7cbebe620a906e3e55c8602d5ab42b24aad571f1
Russell Little <rrlittle2@gmail.com>
2022-09-04 10:33:03 -0500

    hmmmm... it seems I screwed up the add storybook thing

--- apps/cool-app/webpack.config.js
+++ apps/cool-app/webpack.config.js
@@ -6,22 +6,49 @@ module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(
     {
       ...env,
-      babel: { dangerouslyAddModulePathsToTranspile: ["cool-lib"] },
+      babel: { dangerouslyAddModulePathsToTranspile: ["ui"] },
     },
     argv,
   );
 
   if (config.mode === "development") {
     config.devServer.compress = false;
-    config.plugins.push(new ReactRefreshWebpackPlugin());
+    // config.plugins.push(new ReactRefreshWebpackPlugin());
     config.watch = true;
     config.resolve.symlinks = true;
+    config.resolve.byDependency = {
+      esm: {
+        mainFields: ["browser", "module"],
+      },
+      commonjs: {
+        aliasFields: ["browser"],
+      },
+      url: {
+        preferRelative: true,
+      },
+    };
+    config.resolveLoader = {
+      modules: ["node_modules"],
+      extensions: [".js", ".json"],
+      mainFields: ["loader", "main"],
+    };
   }
 
   if (config.mode === "production") {
     config.optimization.minimize = false;
   }
 
+  // seems like the expo config does some bad shtuff...
+  delete config.devServer; // expo dev server has so manu unexpected keys
+  config.devServer = {
+    compress: false,
+    historyApiFallback: { disableDotRule: true, index: "/" },
+    host: "0.0.0.0",
+    hot: true,
+    https: false,
+  };
+  delete config.node; // Configuration has an unknown property
+  console.log(config);
   // Finally return the new config for the CLI to use.
   return config;
 };
