f1778c97c55980baabd8505f63102d3c180183cf
Russell Little <rrlittle2@gmail.com>
2022-09-09 12:19:57 -0500

    added sytem default to theming

--- apps/calculator/src/context/ThemeContext.tsx
+++ apps/calculator/src/context/ThemeContext.tsx
@@ -1,5 +1,6 @@
 import { LightTheme, DarkTheme } from "../themes";
-import { createContext, useCallback, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
+import { Appearance } from "react-native";
 
 export type ThemeContextState = {
   theme: Theme;
@@ -20,7 +21,16 @@ export type ThemeProviderProps = {
 };
 
 export const ThemeProvider = ({ children }: ThemeProviderProps) => {
-  const [themeName, setTheme] = useState("dark");
+  const [themeName, setTheme] = useState(
+    Appearance.getColorScheme() === "light" ? "light" : "dark",
+  );
+
+  useEffect(() => {
+    const subscription = Appearance.addChangeListener((prefs) =>
+      setTheme(prefs.colorScheme === "light" ? "light" : "dark"),
+    );
+    return () => subscription.remove();
+  }, [setTheme]);
 
   const toggleTheme = useCallback(() => {
     setTheme(themeName === "dark" ? "light" : "dark");
